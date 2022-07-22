ee4c6fc8c395e5fc4d63cff1b4af52e3b8fd9a6b
JamesNYim <JamesNYim@gmail.com>
2022-07-21 20:33:08 -0700

    need to fix rolling

--- GachaGachaGame/Assets/Code/Characters.cs
+++ GachaGachaGame/Assets/Code/Characters.cs
@@ -2,17 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cards : MonoBehaviour
+public class Charcters : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    public Dictionary<string, int> characterSet;
+
+    //helper functions
+    private void addValue(Dictionary<string, int> set, string key, int val)
     {
-        
+        set[key] += val;
+        return;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Creating empty character set
+    public void createSet()
     {
-        
+        characterSet = new Dictionary<string, int>();
+        return;
     }
+
+    //getting the character set
+    public Dictionary<string, int> getSet()
+    {
+        return characterSet;
+    }
+
+    //Clearing the character set
+    public void clearSet()
+    {
+        characterSet.Clear();
+        return;
+    }
+
+    //Checking if character set is empty
+    public bool isEmptySet()
+    {
+        if (characterSet.Count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    //Checking if a character is in the character set
+    public bool hasCharacter(string characterName)
+    {
+        return characterSet.ContainsKey(characterName);
+    }
+
+    //Adding a character to character set
+    public void addCharacter(string characterName, int numOfCharacters) //Can i keep the Dictionart<>?
+    {
+        //Checking if the character is already in the set
+        if (this.hasCharacter(characterName)) 
+        {
+            this.addValue(characterSet, characterName, numOfCharacters);
+        }
+        else
+        {
+            characterSet.Add(characterName, numOfCharacters);
+        }
+        return;
+    }
+
+    //Removing a character from character set
+    public bool removeCharacter(string characterName, int numOfCharacters)
+    {
+        if (this.hasCharacter(characterName))
+        {
+            this.addValue(characterSet, characterName, -numOfCharacters);
+            return true;
+        }
+        return false;
+    }
+
+    //Printing character set
+    public void printSet()
+    {
+        foreach (KeyValuePair<string, int> character in characterSet)
+        {
+            Debug.LogFormat("Character Name: {0}, Amount: {1}", character.Key, character.Value);
+        }
+    }
+
+    
 }
+
--- GachaGachaGame/Assets/Code/SaveSystem.cs
+++ GachaGachaGame/Assets/Code/SaveSystem.cs
@@ -3,43 +3,45 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
-
-public static class SaveSystem 
+namespace Game
 {
-    public static void savePlayer(Player player)
+    public static class SaveSystem 
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string filePath = Application.persistentDataPath + "/playerData.data";
-
-        FileStream stream = new FileStream(filePath, FileMode.Create);
-
-        //Writing to file
-        PlayerData data = new PlayerData(player);
-
-        formatter.Serialize(stream, data);
-        stream.Close();
-    }
-
-    public static PlayerData loadPlayer()
-    {
-        string filePath = Application.persistentDataPath + "/playerData.data";
-
-        //checking if file exists
-        if (File.Exists(filePath))
+        public static void savePlayer(Player player)
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(filePath, FileMode.Open);
+            string filePath = Application.persistentDataPath + "/playerData.data";
 
-            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            FileStream stream = new FileStream(filePath, FileMode.Create);
 
+            //Writing to file
+            PlayerData data = new PlayerData(player);
+
+            formatter.Serialize(stream, data);
             stream.Close();
-            return data;
         }
-        else
+
+        public static PlayerData loadPlayer()
         {
-            Debug.Log("Save file was not found in " + filePath);
-            return null;
+            string filePath = Application.persistentDataPath + "/playerData.data";
+
+            //checking if file exists
+            if (File.Exists(filePath))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(filePath, FileMode.Open);
+
+                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+                stream.Close();
+                return data;
+            }
+            else
+            {
+                Debug.Log("Save file was not found in " + filePath);
+                return null;
+            }
         }
+        
     }
-    
 }
