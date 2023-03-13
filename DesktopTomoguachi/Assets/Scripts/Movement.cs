36a800be52625c1726203bdfa0f2fbe3d36357c1
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2023-03-13 15:36:05 -0700

    Movement down

--- DesktopTomoguachi/Assets/Scripts/Movement.cs
+++ DesktopTomoguachi/Assets/Scripts/Movement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    public float moveSpeed = 5;
+    private float currentWaitTime;
+    public float waitTime = 3;
 
-    private Vector2 movement;
+    public Transform waypoint;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    void Start()
+    {
+        currentWaitTime = waitTime;
+        waypoint.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
+        transform.position = Vector2.MoveTowards(transform.position, waypoint.position, moveSpeed * Time.deltaTime);
+        
+        // Checking if we reached our destination
+        if (Vector2.Distance(transform.position, waypoint.position) < 0.2f) 
+        {
+            if (currentWaitTime <= 0)
+            {
+                waypoint.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                currentWaitTime = waitTime;
+            }
+            else
+            {
+                currentWaitTime -= Time.deltaTime;
+            }
+        }
+   }
 }
 // https://www.youtube.com/watch?v=jvtFUfJ6CP8 movement
 // https://youtu.be/RqgsGaMPZTw Transparent window
